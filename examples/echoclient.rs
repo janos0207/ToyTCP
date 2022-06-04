b66454a032a96c16f9f0fe05ba4b4cc6f5715690
y_fukuda <colors92window@gmail.com>
2022-06-05 01:35:20 +0900

    add: ToyTCP defined

--- examples/echoclient.rs
+++ examples/echoclient.rs
@@ -4,14 +4,14 @@ use toytcp::tcp::TCP;
 
 fn main() -> Result<()> {
     let args: Vec<String> = env::args().collect();
-    let addr: Ipv4Addr = args[1].parse();
-    let port: u16 = args[2].parse();
+    let addr: Ipv4Addr = args[1].parse()?;
+    let port: u16 = args[2].parse()?;
     echo_client(addr, port)?;
-    OK(())
+    Ok(())
 }
 
 fn echo_client(remote_addr: Ipv4Addr, remote_port: u16) -> Result<()> {
     let tcp = TCP::new();
     let _ = tcp.connect(remote_addr, remote_port)?;
-    OK(())
+    Ok(())
 }
